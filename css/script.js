fe9e5ca3a53fdc57519bb8d0e339351c80ada2a8
Homonnai Richard <90277977+homonnairichard@users.noreply.github.com>
2024-05-06 16:32:20 +0200

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTopButton = document.querySelector('.scroll-to-top');
+  
+    // Ha a gombra kattintanak, görgetünk az oldal tetejére
+    scrollToTopButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  
+    // Vizsgáljuk, hogy hol van a felhasználó az oldalon, és megjelenítjük vagy elrejtjük a gombot
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > 300) {
+        scrollToTopButton.style.display = 'block';
+      } else {
+        scrollToTopButton.style.display = 'none';
+      }
+    });
+  });
+  
